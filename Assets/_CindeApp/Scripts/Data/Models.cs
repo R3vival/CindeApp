a69a0baa0470c3cb6300b4d3c733c040e5c221fa
R3-Santiago <R3santiago.gu@gmail.com>
2021-05-14 22:44:06 -0500

    Main Data Creation and Setup

---
+++ Assets/_CindeApp/Scripts/Data/Models.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinde {
+    [Serializable]
+    public class User{
+        [SerializeField]private string userName;
+        [SerializeField]private Avatar avatar;
+
+        [SerializeField] public string UserName { get => userName; set => userName = value; }
+        [SerializeField] public Avatar Avatar { get => avatar; set => avatar = value; }
+
+        ///Activitys
+    }
+    [Serializable]
+    public class Avatar {
+        public AvatarComponent[] HeadShape;
+        public AvatarComponent[] Face;
+        public AvatarComponent[] Dress;
+        public Moods moods;
+    }
+    [Serializable]
+    public class AvatarComponent {
+        [SerializeField] private Sprite partSprite;
+
+        public Sprite PartSprite { get => partSprite; set => partSprite = value; }
+    }
+
+    public enum Moods {
+        Happy,
+        Sad,
+        Angry,
+        Idle
+    }
+}
---
+++ Assets/_CindeApp/Scripts/ScriptableObjects/UserScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinde {
+    [CreateAssetMenu(fileName ="User",menuName ="Cinde/UserData")]
+    public class UserScriptableObject : ScriptableObject {
+        public User user;
+    }
+}
