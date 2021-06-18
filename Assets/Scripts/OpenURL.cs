c91b673e6e128252d894f502ae840748783603be
R3-Santiago <R3santiago.gu@gmail.com>
2021-06-17 23:18:54 -0500

    Retrospective 5 Fixes V2

--- Assets/Scripts/OpenURL.cs
+++ Assets/Scripts/OpenURL.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenURL : MonoBehaviour
-{
+public class OpenURL : MonoBehaviour {
     // Start is called before the first frame update
-   public void OpenWithUrl(string url) {
-        switch (url) {
+    public void OpenWithUrl(string storyName) {
+        switch (storyName) {
             case "SnowWhite":
-                Application.OpenURL(url);
+                Application.OpenURL("https://cuentosparadormir.com/cuentos-clasicos/cenicienta");
+                break;
+            case "SleepingBeauty":
+                Application.OpenURL("https://cuentosparadormir.com/cuentos-clasicos/la-bella-durmiente");
+                break;
+            case "BeautyAndTheBeast":
+                Application.OpenURL("https://cuentosparadormir.com/cuentos-clasicos/la-bella-y-la-bestia");
                 break;
         }
-        
     }
 
-    public void AbrirLink1()
-    {
+    public void AbrirLink1() {
         Application.OpenURL("https://www.youtube.com/watch?v=cOzEXFkftrY");
     }
 
-    public void AbrirLink2()
-    {
+    public void AbrirLink2() {
         Application.OpenURL("https://www.youtube.com/watch?v=0_TzTfQnCYo");
     }
 }
