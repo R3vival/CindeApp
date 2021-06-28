c97776fcf4000c00688dbeb035e9d6d68529fb4c
R3-Santiago <R3santiago.gu@gmail.com>
2021-06-28 00:34:49 -0500

    ScreenShoot New Implementation and more Fixes

--- Assets/_CindeApp/Scripts/Controllers/TakeScreenShootController.cs
+++ Assets/_CindeApp/Scripts/Controllers/TakeScreenShootController.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TakeScreenShootController : MonoBehaviour
-{
+public class TakeScreenShootController : MonoBehaviour {
     [SerializeField] int screenShootSize = 1;
-    [SerializeField] GameObject popUpGameObject;
+    [SerializeField] GameObject popUpGameObject, blinkGameObject;
 
     public void HidePanel() {
         popUpGameObject.SetActive(false);
-    }    
-    public void TakeScreenShoot(string fileName) {
-        ScreenCapture.CaptureScreenshot(fileName, screenShootSize);
-        StartCoroutine(ShowPanel());
     }
-    IEnumerator ShowPanel() {
-        yield return new WaitForSeconds(2f);
+    public void TakeScreenShoot(string fileName) {
+        blinkGameObject.SetActive(true);
+#if !UNITY_EDITOR
+        StartCoroutine(ShowPanel(fileName));
+#endif
+    }
+    IEnumerator ShowPanel(string fileName) {
+        yield return new WaitForSeconds(1f);
+        yield return new WaitForEndOfFrame();
+
+        Texture2D texture =  new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24,false);
+
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+
+        string name = fileName +"_"+ System.DateTime.Now.ToString("dd-MM-yyyy") + ".png";
+
+        //ScreenCapture.CaptureScreenshot(fileName + ".png", screenShootSize);
+        NativeGallery.SaveImageToGallery(texture, "CindeApp", name);
+
+        Destroy(texture);
+
+        blinkGameObject.SetActive(false);
         popUpGameObject.SetActive(true);
     }
 }
